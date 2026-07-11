fc379a91f93d806ff443c055117cde15f123b4a7
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-07-11 15:42:32 +0100

    feat: add reusable  text component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui";
+import { Text } from "@/components/ui";
 import Image from "next/image";
 
 export default function Home() {
@@ -14,9 +15,9 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
+          <Text as="p" scale={"heading1"}>
+            To get started, edit the page.tsx file
+          </Text>
           <Button size="sm">SUCK YA MOTHER YOOO!!</Button>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
