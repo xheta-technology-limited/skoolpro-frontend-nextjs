30b6185227c5aca8109a991aad6477b8411be6d2
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-07-13 08:55:32 +0100

    feat: add icon to input field

--- components/ui/form/input/input.tsx
+++ components/ui/form/input/input.tsx
@@ -1,30 +1,55 @@
+"use client";
+import { useFormContext } from "react-hook-form";
+import { XIcon } from "@phosphor-icons/react";
+
 type InputProps = {
-  isError?: boolean;
+  name: string;
+  label?: string;
+  isSuccess?: boolean;
   isLoading?: boolean;
   isWarning?: boolean;
-  placeholder?: string;
-} & React.ButtonHTMLAttributes<HTMLInputElement>;
+  icon?: React.ReactNode;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = ({
-  isError,
+  name,
+  label,
+  isSuccess,
   isLoading,
   isWarning,
-  placeholder,
-  children,
+  icon,
+  ...props
 }: InputProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const error = errors[name]?.message as string | undefined;
+
   return (
-    <>
-      <div className="relative">
-        <input
-          className="w-75 h-[3.18rem] peer rounded-ml bg-[#F5F5FF] px-ml text-[0.875rem] md:text-[1rem] focus:pt-2 focus:bg-transparent focus:border-primary-500"
-          placeholder=" "
-        />
-        <label className="absolute left-ml transition-all peer-focus:top-1 top-4 peer-focus:text-[0.75rem] text-neutral-400">
-          {placeholder}
-        </label>
-        <img className="absolute right-5 top-[0.843rem]" />
-      </div>
-    </>
+    <div style={{ backgroundColor: error && "#FBD6D45C" }} className="relative">
+      <input
+        {...register(name)}
+        {...props}
+        style={{ paddingRight: icon ? "55px" : undefined }}
+        className="w-75 h-[3.18rem] mb-2 peer rounded-ml bg-[#F5F5FF] px-ml text-[0.875rem] md:text-[1rem] focus:pt-2 focus:bg-transparent focus:border-primary-500"
+        placeholder=" "
+      />
+
+      <label className="absolute left-ml top-4 text-neutral-400 transition-all peer-focus:top-1 peer-focus:text-[0.75rem]">
+        {label}
+      </label>
+
+      {icon && <div className="absolute right-5 top-[0.843rem]">{icon}</div>}
+
+      {error && (
+        <>
+          <XIcon size={16} color="#C03744" />{" "}
+          <span className="ml-2 text-xs text-[#C03744]">{error}</span>
+        </>
+      )}
+    </div>
   );
 };
 
