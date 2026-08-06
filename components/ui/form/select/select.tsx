486ed85f7046a15f88a21723ae2afc260f4cb6c2
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-08-06 16:16:17 +0100

    chore: add zod schema helper

--- components/ui/form/select/select.tsx
+++ components/ui/form/select/select.tsx
@@ -147,7 +147,8 @@ const Select = ({
                 className={clsx(
                   icon && "pr-13.75",
                   "pr-13.75",
-                  "w-full h-[3.18rem] mb-2 peer rounded-ml bg-[#F5F5FF] px-ml text-[0.875rem] md:text-[1rem] focus:bg-transparent focus:border-primary-500 not-placeholder-shown:bg-transparent disabled:bg-[#F6F3FDCC] disabled:cursor-not-allowed cursor-pointer"
+                  "w-full h-[3.18rem] mb-2 peer rounded-ml bg-[#F5F5FF] px-ml text-[0.875rem] md:text-[1rem] focus:bg-transparent focus:border-primary-500 not-placeholder-shown:bg-transparent disabled:bg-[#F6F3FDCC] disabled:cursor-not-allowed cursor-pointer",
+                  error && "bg-[#FBD6D45C]"
                 )}
               />
             ) : (
@@ -159,7 +160,8 @@ const Select = ({
                 className={clsx(
                   "pr-13.75",
                   "w-full h-[3.18rem] mb-2 peer rounded-ml bg-[#F5F5FF] px-ml text-left text-[0.875rem] md:text-[1rem] focus:bg-transparent focus:border-primary-500 disabled:bg-[#F6F3FDCC] disabled:cursor-not-allowed cursor-pointer",
-                  selectedOption && "bg-transparent"
+                  selectedOption && "bg-transparent",
+                  error && "bg-[#FBD6D45C]"
                 )}
               >
                 {selectedOption?.label}
