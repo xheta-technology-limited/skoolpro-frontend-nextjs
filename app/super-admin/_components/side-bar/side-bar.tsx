8e6bc9cd8771b6e1ff522e088f8e29e6a4b14122
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-08-23 22:04:44 +0100

    style(sidebar): add responsiveness

--- app/super-admin/_components/side-bar/side-bar.tsx
+++ app/super-admin/_components/side-bar/side-bar.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/config/super-admin-navigation"; // adjust to wherever this file actually lives
 import { getNavIcon } from "./utils";
 import Image from "next/image";
-import { AdmiralBlue11 } from "@/components/icons/logos";
+import { AdmiralBlue11, SkoolproCircle } from "@/components/icons/logos";
 import { Text } from "@/components/ui";
 
 type DashboardSidebarProps = {
@@ -30,15 +30,29 @@ export default function Sidebar({
 
   return (
     <aside
-      className={cn("flex h-screen py-8 w-72 flex-col bg-white", className)}
+      className={cn(
+        "flex h-screen py-4 md:py-8 w-16 md:w-72 flex-col bg-white",
+        className
+      )}
     >
       {/* Logo */}
-      <AdmiralBlue11 height={51} width={199} />
+      <div className="hidden md:flex justify-start">
+        <AdmiralBlue11 height={51} width={199} />
+      </div>
+
+      <div className="flex md:hidden justify-center">
+        <Image
+          alt="skoolpro_logo"
+          width={100}
+          height={25}
+          src={"/icons/SkoolPro_Mignight_Blue_2_1.png"}
+        />
+      </div>
 
       <div className="h-6" />
 
       {/* Nav */}
-      <nav className="flex-1 overflow-y-auto pl-4 pb-6  scrollbar-none [&::-webkit-scrollbar]:hidden">
+      <nav className="flex-1 overflow-y-auto md:pl-4 pb-6  scrollbar-none [&::-webkit-scrollbar]:hidden">
         <ul className="flex flex-col gap-8">
           {items.map((item) => (
             <NavItem
@@ -74,8 +88,8 @@ function NavItem({
       (item.href !== "/dashboard" && pathname.startsWith(item.href + "/")));
 
   const rowClasses = cn(
-    "relative flex w-full items-center text-foreground gap-3 px-4 py-3 transition-all",
-    depth > 0 && "pl-10",
+    "relative flex w-full items-center justify-center md:justify-start text-foreground gap-3 px-4 py-3 transition-all",
+    depth > 0 && "pl-5 md:pl-10",
     isActive
       ? [
           "bg-primary-bg relative text-primary rounded-bl-ml rounded-tl-ml shadow-[-3px_0px_0px_0px_var(--color-primary)]",
@@ -88,7 +102,8 @@ function NavItem({
           "before:h-8.75",
           "before:w-8",
           "before:rounded-br-[18px]",
-          "before:shadow-[0_20px_0_0_#f5f5ff]",
+          "md:before:shadow-[0_20px_0_0_#f5f5ff]",
+          "before:shadow-[0_10px_0_0_#f5f5ff]",
 
           "after:content-['']",
           "after:absolute",
@@ -108,7 +123,7 @@ function NavItem({
       <li>
         <button type="button" onClick={onLogout} className={rowClasses}>
           <Icon className="h-6 w-6 shrink-0" />
-          <span className="truncate">{item.label}</span>
+          <span className="sr-only truncate md:not-sr-only">{item.label}</span>
         </button>
       </li>
     );
@@ -120,7 +135,7 @@ function NavItem({
       <Link href={item.href} className={rowClasses}>
         <Icon className="shrink-0" />
         <Text
-          className="truncate"
+          className="sr-only truncate md:not-sr-only"
           weight={isActive ? "accent" : "standard"}
           scale={"caption"}
         >
