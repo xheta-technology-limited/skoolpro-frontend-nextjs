5e3a94933cdf60becfb781dfde9d962fa5456517
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-08-24 19:32:42 +0100

    fix

--- app/super-admin/layout.tsx
+++ app/super-admin/layout.tsx
@@ -6,12 +6,12 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex">
+    <div className="flex h-screen overflow-hidden">
       <AdminSideBar />
 
-      <main className="w-full flex flex-col">
+      <main className="w-full min-h-0 flex flex-col">
         <StatusBar />
-        <div className="w-full flex-1 pl-12 py-8 pr-20 bg-primary-bg">
+        <div className="w-full flex-1 min-h-0 overflow-y-scroll pl-12 py-8 pr-20 bg-primary-bg">
           <div className="w-full">{children}</div>
         </div>
       </main>
