4eb95f1907e7e544e8219f65a6ebbefaca10a450
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-08-30 17:54:36 +0100

    feat: add subject

--- app/super-admin/school-onboarding/academic-year/_components/create-subjects.tsx
+++ app/super-admin/school-onboarding/academic-year/_components/create-subjects.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { FormProvider, useForm, useFieldArray } from "react-hook-form";
 import { Text } from "@/components/ui";
-import { DatePicker, Input, Select } from "@/components/ui/form";
+import { Checkbox, DatePicker, Input, Select } from "@/components/ui/form";
 import { Button } from "@/components/ui/custom-button";
 import { useSearchParams } from "next/navigation";
 import { AddSquare } from "iconsax-reactjs";
@@ -12,16 +12,51 @@ import { useProgressRouter } from "@/features/page-loader";
 import CatalogTable from "./catalog-table";
 import AssignedSubjectsTable from "./assigned-subjects-table";
 import { SuccessModal } from "@/components/common";
+import { useListStages } from "@/features/academic-year/api/list-stages";
+import { useCreateSubject } from "@/features/academic-year/api/create-subject";
+import {
+  CreateSubjectRequest,
+  createSubjectSchema,
+} from "@/features/academic-year/schemas/create-subject-schema";
+import { setFormErrors } from "@/lib/helpers/set-form-errors";
+import { toast } from "sonner";
+import { useListSubjects } from "@/features/academic-year/api/list-subjects";
+import { Spinner } from "@/components/animations";
 
 export default function CreateSubjects() {
   const router = useProgressRouter();
   const searchParams = useSearchParams();
-  const methods = useForm<any>({
+  const addSubjectMethods = useForm<CreateSubjectRequest>({
+    //placeholder, change when implementing api
+    defaultValues: {},
+    resolver: zodResolver(createSubjectSchema),
+  });
+
+  const assignSubjectMethods = useForm<any>({
     //placeholder, change when implementing api
     defaultValues: {},
     // resolver: zodResolver(),
   });
-  const levelController = useForm<{ class: string }>();
+
+  const { data: stages, isPending: isStagesPending } = useListStages({
+    refetchOnWindowFocus: false,
+  });
+  const stageOptions =
+    stages?.map((stage) => ({ label: stage.name, value: stage.id })) || [];
+
+  const { mutate: createSubjectMutate, isPending: isCreateSubjectPending } =
+    useCreateSubject();
+  const onCreateSubject = (data: CreateSubjectRequest) => {
+    createSubjectMutate(data, {
+      onError: (res) => setFormErrors(addSubjectMethods.setError, res.errors),
+      onSuccess: () => toast.success("Subject added successfully"),
+    });
+  };
+
+  const { data: subjects, isPending: isSubjectsPending } = useListSubjects(
+    undefined,
+    { refetchOnWindowFocus: false }
+  );
 
   const finish = () => {
     router.push(
@@ -36,9 +71,6 @@ export default function CreateSubjects() {
   const onCloseSuccessModal = () =>
     router.push("/super-admin/school-onboarding/academic-year");
 
-  const addSubject = () => {
-    alert("Do some API stuff");
-  };
   const assignSubject = () => {
     alert("Also do some API stuff");
   };
@@ -56,25 +88,32 @@ export default function CreateSubjects() {
           <Text className="text-neutrals-700" scale={"content"}>
             Add a subject
           </Text>
-          <FormProvider {...methods}>
+          <FormProvider {...addSubjectMethods}>
             <form
               id="add-subject-form"
               className="sm:h-auto gap-4 w-full grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))]"
-              onSubmit={methods.handleSubmit(addSubject)}
+              onSubmit={addSubjectMethods.handleSubmit(onCreateSubject)}
             >
-              <Input name="arm_name" label="Arm Name" />
-              <Input name="arm_code" label="Arm Code" />
-              <Select name="name" options={DUMMY_CAMPUSES} label="Campus" />
+              <Input name="name" label="Enter subject name" />
+              <Input name="code" label="Enter subject code" />
               <Select
-                name="code"
-                options={DUMMY_CLASS_TEACHERS}
-                label="Class Teacher"
+                name="department"
+                options={[]}
+                label="Select department"
+              />
+              <Checkbox
+                name="intended_stage_ids"
+                options={stageOptions}
+                label="Intended stage"
+                isLoading={isStagesPending}
+                isLoadingText="Fetching stages"
               />
             </form>
             <Button
               type="submit"
               form="add-subject-form"
               variant="secondary"
+              loading={isCreateSubjectPending}
               size="sm"
               className="justify-self-end max-w-fit ml-auto"
             >
@@ -88,19 +127,17 @@ export default function CreateSubjects() {
               </Text>
             </Button>
           </FormProvider>
-          <Text className="text-neutrals-700" scale={"content"}>
-            Subject catalog
-          </Text>
-          <CatalogTable />
+
+          <CatalogTable data={subjects} isSubjectsPending={isSubjectsPending} />
           <>
             <Text className="text-neutrals-700" scale={"content"}>
               Assign subject to a level
             </Text>
-            <FormProvider {...methods}>
+            <FormProvider {...addSubjectMethods}>
               <form
                 id="assign-subject-form"
                 className="sm:h-auto gap-4 w-full grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(300px,1fr))]"
-                onSubmit={methods.handleSubmit(assignSubject)}
+                onSubmit={assignSubjectMethods.handleSubmit(assignSubject)}
               >
                 <Select name="name" options={DUMMY_CAMPUSES} label="Campus" />
                 <Select
