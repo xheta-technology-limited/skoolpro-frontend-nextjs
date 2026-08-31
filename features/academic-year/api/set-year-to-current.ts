7322ec8eac7d98f4f7668df8e95f9c75dcd785c0
Taterbro <135230103+Taterbro@users.noreply.github.com>
2026-08-31 11:51:29 +0100

    chore: create set year to current

--- features/academic-year/api/set-year-to-current.ts
+++ features/academic-year/api/set-year-to-current.ts
@@ -1,17 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { api } from "@/lib/api";
-import { academicYearKeys, armKeys } from "./query-keys";
+import { academicYearKeys } from "./query-keys";
 import { ServerErrorResponse } from "@/types/api";
 
-export const setCurrentTerm = (termID: string): Promise<null> => {
-  return api.post(`academic-terms/${termID}/set-current`, {});
+export const setCurrentYear = (yearID: string): Promise<null> => {
+  return api.post(`academic-years/${yearID}/set-current`, {});
 };
 
-export const useSetCurrentTerm = (termID: string) => {
+export const useSetCurrentYear = (yearID: string) => {
   const queryClient = useQueryClient();
 
   return useMutation<null, ServerErrorResponse, {}>({
-    mutationFn: () => setCurrentTerm(termID),
+    mutationFn: () => setCurrentYear(yearID),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: academicYearKeys.all });
     },
