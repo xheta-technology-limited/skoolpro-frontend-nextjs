cff22522591fd5f564614aae40ee509c77e15af7
Victor Alfa <135230103+Taterbro@users.noreply.github.com>
2026-09-07 23:09:07 +0100

    feat: add user management header (#76)
    * feat: add user mgt header
    
    * style: interactive buttons

--- app/super-admin/user-management/_components/card.tsx
+++ app/super-admin/user-management/_components/card.tsx
@@ -1,8 +1,8 @@
+"use client";
 import { ArrowRight3, UserOctagon } from "iconsax-reactjs";
 import { Text } from "@/components/ui";
-import Link from "next/link";
 import { Group } from "../constants";
-
+import { useProgressRouter } from "@/features/page-loader";
 import { cn } from "@/lib/utils";
 
 interface Props {
@@ -11,8 +11,13 @@ interface Props {
 }
 
 export default function ManagementCard({ group, className }: Props) {
+  const router = useProgressRouter();
   return (
-    <Link href={group.pageUrl} className={cn("block", className)}>
+    <button
+      onClick={() => router.push(group.pageUrl)}
+      className={cn("text-left", className)}
+      aria-label={`${group.role} management`}
+    >
       <div className="bg-white rounded-ml p-4">
         <div className="flex items-center gap-4">
           <div className="h-11.25 w-11.25 bg-base-white rounded-[9px] p-2.5">
@@ -38,6 +43,6 @@ export default function ManagementCard({ group, className }: Props) {
           <ArrowRight3 variant="Bulk" size={24} className="text-primary" />
         </div>
       </div>
-    </Link>
+    </button>
   );
 }
