a9dd94ad83d1b190e16c03eadc7fd429f1c1ab6a
Victor Alfa <135230103+Taterbro@users.noreply.github.com>
2026-09-10 08:51:46 +0100

    feat: add status badge (#79)

--- components/common/index.ts
+++ components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as SuccessModal } from "./successModal/modal";
 export { default as ModuleSearch } from "./module-search/module-search";
 export { default as MiniSelector } from "./mini-select/mini-select";
+export { default as StatusBadge } from "./status-badge/status-badge";
