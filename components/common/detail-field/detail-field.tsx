d72b0a060a62d1eba8f535fc60ab6e020c6301f4
Victor Alfa <135230103+Taterbro@users.noreply.github.com>
2026-09-11 10:22:47 +0100

    feat: add detail field (#82)

--- components/common/detail-field/detail-field.tsx
+++ components/common/detail-field/detail-field.tsx
@@ -4,7 +4,7 @@ interface DetailFieldProps {
 }
 
 const DetailField = ({ label, value }: DetailFieldProps) => (
-  <div className="flex min-h-[55px] overflow-hidden flex-1 flex-col gap-2 rounded-[8px] border border-grays-borders bg-white p-2">
+  <div className="flex min-h-13.75 overflow-hidden flex-1 flex-col gap-2 rounded-[8px] border border-grays-borders bg-white p-2">
     <span className="text-[12px] font-normal leading-[1.2] text-neutrals-700">
       {label}
     </span>
--- components/common/index.ts
+++ components/common/index.ts
@@ -2,3 +2,4 @@ export { default as SuccessModal } from "./successModal/modal";
 export { default as ModuleSearch } from "./module-search/module-search";
 export { default as MiniSelector } from "./mini-select/mini-select";
 export { default as StatusBadge } from "./status-badge/status-badge";
+export { default as DetailField } from "./detail-field/detail-field";
